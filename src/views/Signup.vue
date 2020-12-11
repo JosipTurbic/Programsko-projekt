aaedfd4efae925e6dfedc6c3cb80ae6b0c3d1190
Josip <jturbic@unipu.hr>
2020-12-11 15:21:41 +0100

    Postavljanje firebasea

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="about">
-  <h1>This is an signup page</h1>
+  <h1>Registriraj se!</h1>
 <div class="container">
   <div class="row">
     <div class="col-sm">
@@ -8,28 +8,42 @@
       <div class="col-sm">
         <form>
           <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
+            <label for="exampleInputEmail1">Email adresa</label>
             <input 
             type="email" 
+            v-model="username"
             class="form-control" 
             id="exampleInputEmail1" 
             aria-describedby="emailHelp" 
             placeholder="Enter email"
             />
             <small id="emailHelp" class="form-text text-muted">
-              We'llnever share your email with anyone else.
+              Shh.. Nikome nećemo reći tvoju lozinku.
               </small>
               </div>
               <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
+                <label for="exampleInputPassword1">Lozinka</label>
                 <input
                 type="password" 
+                v-model="password"
                 class="form-control"
                 id="exampleInputPassword1"
-                placeholder="Password"
+                placeholder="Lozinka"
                 />
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <div class="form-group">
+                  <label for="exampleInputPassword2">Ponovljena lozinka</label>
+                <input 
+            type="password" 
+            v-model="passwordRepeat"
+            class="form-control" 
+            id="exampleInputPassword2" 
+            aria-describedby="eHelp" 
+            placeholder="Ponovite lozinku"
+            />
+            </div>
+
+                <button type="button" @click="signup" class="btn btn-primary">Pošalji</button>
                 </form>
                 </div>
                 <div class="col-sm">
@@ -38,3 +52,34 @@
                   </div>
                   </div>
                   </template>
+
+<script>
+  import {firebase} from '@/firebase';
+
+export default {
+  name: "Signup",
+  data(){
+    return{
+      username: '',
+      password: '',
+      passwordRepeat: '',
+    };
+  },
+  methods: {
+      signup(){
+        firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.username, this.password)
+        .then(
+          function()
+            {
+              console.log("Uspješna registracija");
+            })
+            .catch(function(error){
+              console.error("Došlo je do greške!", error)
+            });
+            console.log('Nastavak');
+      },
+  },
+};
+</script>
