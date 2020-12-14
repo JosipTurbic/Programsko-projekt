a40474ccd071b69c43bb202464097269d605396a
Josip <jturbic@unipu.hr>
2020-12-14 19:37:00 +0100

    Uredena pocetna

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,37 @@
 <template>
   <div>
- 
-  </div>
+    <div>
+    <img src="@/assets/teretana_002.jpg" width="100%" height="350"/>
+    </div>
+  <div class="container"  height="1050">
+  <div class="row" >
+    <div class="col align-self-start" id="slova">
+      Pronađi željene vježbe
+    </div>
+    <div class="col align-self-center" id="slova">
+      Napravi vlastiti plan vježbanja
+    </div>
+    <div class="col align-self-end" id="slova">
+      Izmjeri indeks tjelesne mase
+    </div>
+  </div> 
+  </div>   
+</div>
+  
+
+
 </template>
-<script>
+
+<style >
+#slova {
+  
+ font-weight: bold;
+  color: #070707;
+
+}
+</style>
 
 
-    </script>
+
+
+
