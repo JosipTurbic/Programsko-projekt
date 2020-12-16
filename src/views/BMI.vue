0e908bc952077216d9005049399bfdead2bf769f
Josip <jturbic@unipu.hr>
2020-12-16 16:55:32 +0100

    Nedovrseni BMI

--- src/views/BMI.vue
+++ src/views/BMI.vue
@@ -1,5 +1,51 @@
 <template>
+<div>
+ <div>
+    <img src="@/assets/teretana_002.jpg" width="100%" height="350"/>
+    <p>Izmjerite svoj index tjelesne težine</p>
+    </div>
+  
+  <div class="container" id="ulijevo" >
+  <div class="row">
+    <div class="col-sm">
+      </div>
+      <div class="col-sm">
+        <form>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Vaša visina</label>
+            <input 
+            type="text" 
+            class="form-control" 
+            id="exampleInputEmail1" 
+            aria-describedby="emailHelp" 
+            placeholder="Unesite svoju visinu"
+            />
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Vaša težina</label>
+                <input
+                type="text" 
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Unesite svoju težinu"
+                />
+                </div>
+                <button type="button" onclick="" class="btn btn-primary">Izračunaj</button>
+                </form>
+                </div>
+                <div class="col-sm">
+                  </div>
+                  </div>
+                  </div>
+</div>
 
-<h1>AAAA</h1>
+</template> 
+<script>
 
-</template>
+
+</script>
+<style>
+#ulijevo{
+    float:left;
+}    
+</style>
