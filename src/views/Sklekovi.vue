86f74cc187eb041a8fb009a7b10950d990c91ac2
M4T30-772 <mateopakter772@gmail.com>
2021-01-06 15:48:21 +0100

    svi fileovi dodani

---
+++ src/views/Sklekovi.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p> 
+
+        </p>
+        <h3> Kako pravilno izvesti vježbu </h3>
+        <p> 
+
+        </p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Trbusnjaci.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p> Trbušnjaci su savršena vježba za ojačavanje "core-a" i ojačavanja trbušnih mišića koji su jedni od najbitnijih skupina mišića na tijelu </p>
+        <h3> Kako pravilno izvesti vježbu </h3>
+        <p> Lezite na pod, i laktovima se podignite u zrak. Držite se laktovima
+            a ruke skpojite ispod glave i <b> stistnite trbuh </b>. Nakon samo 30 sekunudi
+            trebate osjetiti jako zatezanje trbušnog zida.
+        </p>
+        </div>
+</template>
+
+<style scoped>
+
+</style>
