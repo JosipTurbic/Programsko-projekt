4b7fc987e5ee22971031869257c4ad7a533f2a67
Josip <jturbic@unipu.hr>
2021-02-02 12:06:14 +0100

    Mojprogram needs user

--- src/router/index.js
+++ src/router/index.js
@@ -55,7 +55,7 @@ const routes = [
     path: '/Program',
     name: 'Program',
     meta: {
-      needsUser: false,
+      needsUser: true,
     },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
